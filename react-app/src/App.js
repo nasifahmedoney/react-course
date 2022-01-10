b8043f8249eb0bdc3fa1abaf9225a95f16284040
nasifahmedoney <nasif.ahmed@live.com>
2022-01-10 14:51:54 +0600

    using components and props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,14 @@
+import Todo from './components/Todo';
+
 function App() {
-  return <div>nasif</div>;
+  return (
+    <div>
+      <h1>Todos</h1>
+      <Todo text="title1" />
+      <Todo text="title2" />
+      <Todo text="title3" />
+    </div>
+  );
 }
 
 export default App;
